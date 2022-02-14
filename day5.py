2a1370a990f5245fc67008325fbe724a31031778
shreyasicodes20 <97606334+shreyasicodes20@users.noreply.github.com>
2022-02-14 11:15:49 +0530

    Create day5.py

---
+++ day5.py
@@ -0,0 +1,66 @@
+1)#python program to find a square root of a given numbers
+ 
+a=int(input())
+print(a**2)
+ 
+2)#python program to find a cube root of a given numbers
+ 
+a=int(input())
+print(a**3)
+ 
+3.#python program to find the area of a circle
+ 
+import math
+r=int(input())
+ 
+pie=math.pi
+print(pie*r*r)
+ 
+4.#python program to find the largest number among the three numbers
+ 
+A=int(input())
+B=int(input())
+C=int(input())
+if A>B and A>C:
+ print(A)
+elif B>A and B>C:
+ print(B)
+else:
+ print(C)
+ 
+5.#python program to find the smaller number among the two numbers
+ 
+a=int(input())
+b=int(input())
+if a<b:
+ print(a)
+else:
+ print(b)
+ 
+6.#python program to Print first five numbers of the given N multiple numbers
+ 
+N=int(input())
+for a in range(1,6):
+print(a*N)
+ 
+7.#python program to  find area of the rectangle
+ 
+x=int(input())
+y=int(input())
+A=x*y
+print(A)
+ 
+8.#python program to find the simple interest
+ 
+p=int(input())
+r=int(input())
+t=int(input())
+si=(p*r*t)/100
+print(si)
+ 
+10.#python program to  get the input and print it 5 times.
+ 
+text=input()
+for a in range (5):
+ print(text)
+ 
